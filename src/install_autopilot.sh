572b66748886f4ff49636a86a35b1f5552095f3a
reinzler <lzlzdflz@gmail.com>
2025-03-20 11:50:37 +0300

    install scrip is updated

--- src/install_autopilot.sh
+++ src/install_autopilot.sh
@@ -37,9 +37,20 @@ git clone https://github.com/PX4/px4_msgs.git
 git clone https://github.com/PX4/px4_ros_com.git
 cd ..
 colcon build
-source ~/.bashrc
+source install/local_setup.bash  # <-- Это гарантирует, что новые пакеты доступны
+echo "source $(pwd)/install/local_setup.bash" >> ~/.bashrc
+cd -
 
-# 6. Клонирование Pangolin
+# 6. Установка PX4 keyboard teleop
+echo "Клонирование px4_keyboard_teleop_control"
+git clone https://github.com/DCVAM/px4_keyboard_teleop_control.git
+cd px4_keyboard_teleop_control
+colcon build
+source install/local_setup.bash  # <-- Это гарантирует, что новые пакеты доступны
+echo "source $(pwd)/install/local_setup.bash" >> ~/.bashrc
+cd -
+
+# 7. Клонирование Pangolin
 echo "Клонирование Pangolin..."
 git clone https://github.com/stevenlovegrove/Pangolin
 cd Pangolin
@@ -50,7 +61,7 @@ cmake --build build -j$(nproc)
 sudo cmake --install build
 cd ../..
 
-# 7. Проверка и настройка Настройка LD_LIBRARY_PATH
+# 8. Проверка и настройка Настройка LD_LIBRARY_PATH
 echo "Проверка и настройка LD_LIBRARY_PATH..."
 if [[ ":$LD_LIBRARY_PATH:" != *":/usr/local/lib:"* ]]; then
     export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
@@ -73,7 +84,7 @@ fi
 sudo ldconfig
 source ~/.bashrc
 
-# 8. Клонирование ORB-SLAM_3_ROS2
+# 9. Клонирование ORB-SLAM_3_ROS2
 echo "Клонирование ORB-SLAM_3_ROS2..."
 mkdir -p ~/ros2_test/src
 git clone https://github.com/reinzler/ros2_orb_slam3.git
@@ -84,7 +95,7 @@ source /opt/ros/humble/setup.bash
 colcon build --symlink-install --cmake-args=-DCMAKE_BUILD_TYPE=Release --parallel-workers $(nproc)
 
 
-# 9. Завершение
+# 10. Завершение
 echo "Установка завершена! Все компоненты находятся в папке ~/drone_autopilot."
 echo "Чтобы запустить PX4 SITL, выполните:"
 echo "cd ~/drone_autopilot/PX4-Autopilot && make px4_sitl gz_x500_gimbal_windy"
